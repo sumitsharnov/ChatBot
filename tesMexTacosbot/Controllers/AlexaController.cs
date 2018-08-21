ddb3095b557af4957f21b0cad00c780e737f6edd
sumit <sumitsharnov@gmail.com>
2018-08-21 16:19:19 +0530

    chat bot first phase

---
+++ tesMexTacosbot/Controllers/AlexaController.cs
@@ -0,0 +1,35 @@
+﻿using Alexa.NET.Request;
+using Alexa.NET.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace tesMexTacosbot.Controllers
+{
+    public class AlexaController : ApiController
+    {
+        public SkillResponse post(SkillRequest skillRequest)
+        {
+            var response = new SkillResponse()
+            {
+                Version = "2.0",
+                Response = new ResponseBody()
+            };
+
+            response.Response.OutputSpeech = new PlainTextOutputSpeech()
+            {
+                Text = "Hello Sumit"
+            };
+            return response;
+        }
+        public string Get()
+        {
+            return "Hello Alexa!";
+        }
+
+    }
+}
+
---
+++ tesMexTacosbot/Controllers/DialogFlowController.cs
@@ -0,0 +1,31 @@
+﻿using ApiAiSDK.Model;
+using Google.Apis.Dialogflow.v2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace tesMexTacosbot.Controllers
+{
+    public class DialogFlowController : ApiController
+    {
+        public dynamic Post(AIResponse aiResponse)
+        {
+            var response = new
+            {
+                fulfillmentText = "Hello Sumit",
+                ////Text = "Hello!!"
+            };
+
+            return response;
+
+        }
+
+        public string Get()
+        {
+            return "Hello Google!";
+        }
+    }
+}
