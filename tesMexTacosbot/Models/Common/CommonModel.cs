5a9ec7dfce4e01fab26d44e1d1d5e929416227da
sumit <sumitsharnov@gmail.com>
2018-08-22 13:06:26 +0530

    Common model

---
+++ tesMexTacosbot/Models/Common/CommonModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tesMexTacosbot.Models.Common
+{
+    public class CommonModel
+    {
+        public CommonModel()
+        {
+            Id = "";
+            Session = new Session();
+            Request = new Request();
+            Response = new Response();
+             
+        }
+
+        public string Id { get;  set; }
+        internal Session Session { get; private set; }
+        internal Request Request { get; private set; }
+        internal Response Response { get; private set; }
+    }
+
+    internal class Response
+    {
+        public string Text { get; set; }
+    }
+
+    internal class Request
+    {
+        public string Id { get; set; }
+        public string Intent { get; set; }
+        public List<KeyValuePair<string, string>> Parameters { get; set; }
+    }
+
+    internal class Session
+    {      
+            public string Id { get; set; }
+        
+    }
+}
